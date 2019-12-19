4caba652580faa1dfa5ea6cc92b3fcc673821aed
William Chapman <g.william.chapman.iv@gmail.com>
2019-12-19 17:10:24 -0500

    Remove commented out code

--- +Pippin/+Predictors/PhaseLocking.m
+++ +Pippin/+Predictors/PhaseLocking.m
@@ -5,20 +5,12 @@ function self = PhaseLocking(self, freqRange)
         signal = self.data.b_lfp(self.data.active_lfp).signal;
         fs = self.data.b_lfp(self.data.active_lfp).fs;
         
-        %TODO: decide if fir1 or butterworth
-        %%{
         fNq = fs/2;
         Wn = freqRange / fNq;
         filt = fir1(100,Wn);
         signal_filt = filtfilt(filt, 1, signal);
         phi = angle(hilbert(signal_filt));
-        %}
         
-        %{
-        signal_filt = CMBHOME.LFP.BandpassFilter(signal, fs, freqRange);
-        phi = CMBHOME.LFP.InstPhase(signal_filt);
-        %phi = phi(CMBHOME.Utils.ContinuizeEpochs(self.data.p_lfp_ind(:)));
-        %}
         
         phi = phi(:);
         self.predictors(end+1).name = 'PhaseLocking';
